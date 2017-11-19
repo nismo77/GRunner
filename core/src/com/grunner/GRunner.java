d7425c786706f8503ebe69749e46d96f8a04aaa3
nism0 <nismo.sdt@gmail.com>
2017-11-19 14:34:38 +0100

    Global BitmapFont font added

--- core/src/com/grunner/GRunner.java
+++ core/src/com/grunner/GRunner.java
@@ -1,37 +1,34 @@
 package com.grunner;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GRunner extends ApplicationAdapter {
+public class GRunner extends Game {
 	SpriteBatch batch;
 	Texture img;
+	BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
-		
-		// tcguyjadsadsadsadsadsa
-		
-		// comment from mac!
+	
 	}
 }
