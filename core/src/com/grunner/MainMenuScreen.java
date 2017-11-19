1424c220b5c7367b9682ee85aa647100b3613500
nism0 <nismo.sdt@gmail.com>
2017-11-19 14:31:57 +0100

    Added MainMenuScreen

---
+++ core/src/com/grunner/MainMenuScreen.java
@@ -0,0 +1,71 @@
+package com.grunner;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class MainMenuScreen implements Screen{
+	
+	final GRunner game;
+	OrthographicCamera camera;
+
+	public MainMenuScreen(final GRunner grunner) {
+		game = grunner;
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 600);
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0.0f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		camera.update();
+		game.batch.setProjectionMatrix(camera.combined);
+		
+		game.batch.begin();
+		game.font.draw(game.batch, "Welcome in GRUnner!", 100,100);
+		game.batch.end();
+
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
