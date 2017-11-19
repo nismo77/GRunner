8fb4fc181d158b03525de8f8f1cb9e201947f749
nism0 <nismo.sdt@gmail.com>
2017-11-19 13:56:04 +0100

    Pierwszy commit. Dodano pliki projektu

---
+++ desktop/src/com/grunner/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.grunner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.grunner.GRunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		config.resizable = true;
+		new LwjglApplication(new GRunner(), config);
+		// some commentasdasd
+	}
+	
+}
