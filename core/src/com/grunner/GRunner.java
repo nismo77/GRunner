836badca0b028ec390c613ede87a1e92c0b98afd
nism0 <nismo.sdt@gmail.com>
2017-11-26 22:22:11 +0100

    Added flipping the texture with space

--- core/src/com/grunner/GRunner.java
+++ core/src/com/grunner/GRunner.java
@@ -1,3 +1,5 @@
+package com.grunner;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -10,10 +12,9 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class GRunner extends ApplicationAdapter {
 
 	private final int TEX_ROW = 5, TEX_COL = 6;
-	private boolean start_ani = false;
-	
-	public int ani_x = 100;
+	private boolean start_ani = true, gravity_up = false;
 
+	public int ani_x = 100, ani_y = 50;
 
 	private float ani_time;
 
@@ -23,39 +24,32 @@ public class GRunner extends ApplicationAdapter {
 	private TextureRegion currentFrame;
 	private Object[] frameRegion;
 	private TextureRegion lastFrame;
-	
 
 	@Override
 	public void create() {
-		batch = new SpriteBatch();		
+		batch = new SpriteBatch();
 		ani_time = 0.0f;
 		runnerAtlas = new TextureAtlas(Gdx.files.internal("runner_pack.pack"));
-		anim = new Animation<TextureRegion>(1/30f, runnerAtlas.getRegions());
-		frameRegion = anim.getKeyFrames();  // <-- get all frames into 1D array
+		anim = new Animation<TextureRegion>(1 / 30f, runnerAtlas.getRegions());
+		frameRegion = anim.getKeyFrames(); // <-- get all frames into 1D array
 		lastFrame = (TextureRegion) frameRegion[0];
-
 		
+		ani_time += Gdx.graphics.getDeltaTime();
+		currentFrame = anim.getKeyFrame(ani_time, true);
+
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0.3f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		batch.begin();
-		ani_time += Gdx.graphics.getDeltaTime();
+	
+		batch.draw(currentFrame, ani_x, ani_y);
 
-		if(start_ani) {
-			currentFrame = anim.getKeyFrame(ani_time, true);
-			lastFrame = currentFrame;
-		}
-		else {
-			currentFrame = lastFrame;
-
-		}
-		batch.draw( currentFrame, ani_x, 100);
 		batch.end();
-		
+
 		logic();
 	}
 
@@ -65,23 +59,40 @@ public class GRunner extends ApplicationAdapter {
 		runnerAtlas.dispose();
 
 	}
-	
-	
+
 	public void logic() {
+
+		ani_time += Gdx.graphics.getDeltaTime();
+		currentFrame = anim.getKeyFrame(ani_time, true);
 		
-		if( Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+		ani_x += 100 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
 			Gdx.app.exit();
 		}
-		if( Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			start_ani = true;
-			ani_x += 200 * Gdx.graphics.getDeltaTime();
-			if( ani_x > Gdx.graphics.getWidth() ) {
+		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+			gravity_up = true;
+
+		}
+
+		if (gravity_up) {
+			ani_x += 100 * Gdx.graphics.getDeltaTime();
+			ani_y += 200 * Gdx.graphics.getDeltaTime();
+			if (ani_x > Gdx.graphics.getWidth()) {
 				ani_x = -30;
 			}
+			if (ani_y > Gdx.graphics.getHeight()) {
+				ani_y = 30;
+			}
+
+			// flip the frame
+			if (currentFrame.isFlipY() == false) {
+				currentFrame.flip(false, true);
+			}
+
 		}
-		else {
-			start_ani = false;
-			
-		}
+		System.out.printf("ani_X = %d\nani_y = %d\n\n", ani_x, ani_y);
+		System.out.printf("flipped = %b\n", currentFrame.isFlipY());
+
+
 	}
 }
