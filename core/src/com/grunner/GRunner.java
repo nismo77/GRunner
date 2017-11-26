3b5eb972ba0b0c22148f2354a50fd77113257797
nism0 <nismo.sdt@gmail.com>
2017-11-26 20:01:56 +0100

    Ended up with the right animation presence

--- core/src/com/grunner/GRunner.java
+++ core/src/com/grunner/GRunner.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class GRunner extends ApplicationAdapter {
@@ -19,32 +20,29 @@ public class GRunner extends ApplicationAdapter {
 
 	private float ani_time;
 
-	SpriteBatch batch;
-	Texture animation_sheet;
-	Animation animation;
+	private SpriteBatch batch;
+	private Animation<TextureRegion> anim;
+	private TextureAtlas runnerAtlas;
+	private Texture currentFrame;
 
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-
-		
+		batch = new SpriteBatch();		
 		ani_time = 0.0f;
+		runnerAtlas = new TextureAtlas(Gdx.files.internal("runner_pack.pack"));
+		anim = new Animation<TextureRegion>(1/30f, runnerAtlas.getRegions());
+		
+		
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0.3f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
 		ani_time += Gdx.graphics.getDeltaTime();
-		
-		if (start_ani) {
-			crr_frame = animation.getKeyFrame(ani_time, true);
-		}
-		else {
-			crr_frame = ani_frames[10];
-		}
-		batch.draw(crr_frame, ani_x, 100);
+		batch.draw( anim.getKeyFrame(ani_time, true), ani_x, 100);
 		batch.end();
 		
 		logic();
@@ -53,13 +51,16 @@ public class GRunner extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		batch.dispose();
-		animation_sheet.dispose();
+		runnerAtlas.dispose();
 
 	}
 	
 	
 	public void logic() {
 		
+		if( Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+			Gdx.app.exit();
+		}
 		if( Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
 			start_ani = true;
 			ani_x += 200 * Gdx.graphics.getDeltaTime();
